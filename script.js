4777e9a742adac57c0e1149a21c729a03c7c1139
Iversson.Estudos <iversson.estudos@gmail.com>
2024-05-24 10:33:50 -0300

    again

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+
+function Book(name, pages, author, publicationDate){
+    this.name = name;
+    this.pages = pages;
+    this.author = author;
+    this.publicationDate = publicationDate;
+    this.read = 0;
+}
